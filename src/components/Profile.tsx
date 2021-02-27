5ab57cef0ad48ebf9d1d2188ec59acf5c97c919d
Vitor Quadros <viitorquadros@hotmail.com>
2021-02-27 19:56:04 -0300

    refactor (styles): home css

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+const Profile = () => (
+  <div>
+    <img src="https://github.com/vitorquadros.png" alt="Profile" />
+    <div>
+      <strong>Vitor Quadros</strong>
+      <p>Level: 1</p>
+    </div>
+  </div>
+);
+
+export default Profile;
