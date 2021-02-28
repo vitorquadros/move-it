41ccd08ca43f112bf52be98015375e835e1fda54
Vitor Quadros <viitorquadros@hotmail.com>
2021-02-28 01:57:39 -0300

    feat: countdown component

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,21 @@
+import styles from '../styles/components/Countdown.module.css';
+
+const Countdown = () => (
+  <div>
+    <div className={styles.countdownContainer}>
+      <div>
+        <span>2</span>
+        <span>5</span>
+      </div>
+      <span>:</span>
+      <div>
+        <span>0</span>
+        <span>0</span>
+      </div>
+    </div>
+
+    <button type="button" className={styles.countdownButton}>Iniciar um ciclo</button>
+  </div>
+);
+
+export default Countdown;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import Head from 'next/head';
 import CompletedChallenges from '../components/CompletedChallenges';
+import Countdown from '../components/Countdown';
 import ExperienceBar from '../components/ExperienceBar';
 import Profile from '../components/Profile';
 import styles from '../styles/pages/Home.module.css';
@@ -6,11 +8,15 @@ import styles from '../styles/pages/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio │ move.it</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
 
         <div />
