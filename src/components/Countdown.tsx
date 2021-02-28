3d318a479f92897c2971140544d41f2566352e60
Vitor Quadros <viitorquadros@hotmail.com>
2021-02-28 02:32:37 -0300

    feat (countdown): time state

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,21 +1,29 @@
+import { useState } from 'react';
 import styles from '../styles/components/Countdown.module.css';
 
-const Countdown = () => (
-  <div>
-    <div className={styles.countdownContainer}>
-      <div>
-        <span>2</span>
-        <span>5</span>
+export default function Countdown() {
+  const [time, setTime] = useState(25 * 60);
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+  return (
+    <div>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
       </div>
-      <span>:</span>
-      <div>
-        <span>0</span>
-        <span>0</span>
-      </div>
+
+      <button type="button" className={styles.countdownButton}>Iniciar um ciclo</button>
     </div>
-
-    <button type="button" className={styles.countdownButton}>Iniciar um ciclo</button>
-  </div>
-);
-
-export default Countdown;
+  );
+}
