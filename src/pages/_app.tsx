f841252803bedac47c0cb6366971c484d3ff8d7c
Vitor Quadros <viitorquadros@hotmail.com>
2021-03-04 03:40:24 -0300

    getting cookies from server side

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
 import '../styles/global.css';
-import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
+    <Component {...pageProps} />
   );
 }
 
